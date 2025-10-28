958e2b3ac06341a8eb77978c65947d602588f087
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2025-10-28 11:58:29 +0100

    pydatic-setup-completed

--- schemas.py
+++ schemas.py
@@ -3,3 +3,23 @@ from datetime import datetime
 
 class UserCreate(BaseModel):
     email: EmailStr
+
+class UserRead(BaseModel):
+    id: int 
+    email: EmailStr
+    is_active: bool
+    created_at: datetime
+    updated_at: datetime
+
+class StoreCreate(BaseModel):
+    user_id: int
+    shop_domain: str
+    access_domain: str
+
+class StoreRead(BaseModel):
+    id: int
+    user_id: int
+    shop_domain: str 
+    created_at: datetime
+    updated_at: datetime
+
