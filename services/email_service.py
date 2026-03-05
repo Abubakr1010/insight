b91c91d95d24d168e950826967ed1ab18166d406
Muhammad Abubakr <108617749+Abubakr1010@users.noreply.github.com>
2026-03-05 19:38:35 +0100

    mail-api

--- services/email_service.py
+++ services/email_service.py
@@ -1,5 +1,19 @@
 import os
+from typing import Dict
+from fastapi import APIRouter
 import resend
 
-
 resend.api_key = os.environ["RESEND_API_KEY"]
+
+router = APIRouter(tags=["email"])
+
+@router.post("/mail")
+async def send_mail() -> Dict:
+    params: resend.Emails.SendParams = {
+        "from": "Insight <abubakriqbal1997@gmail.co>",
+        "to": ["delivered@resend.dev"],
+        "subject": "hello world",
+        "html": "<strong>it works!</strong>",
+    }
+    email = await resend.Emails.send_async(params)
+    return email
